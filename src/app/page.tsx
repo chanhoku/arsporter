35418c3fc706f0f72d73dda30e45bfdb1750efd1
chanhoku <>
2023-09-16 19:44:36 +0900

    #first

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "src/app/components/header";
+import Footer from "src/app/components/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
